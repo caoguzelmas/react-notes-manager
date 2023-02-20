c6c895ef2a2f5f7437d85a05cb2e4085074f47c2
caoguzelmas <caoguzelmas@gmail.com>
2023-02-20 18:25:37 +0300

    searchBar component implemented

--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -1,12 +1,11 @@
 import s from "./style.module.css";
 import { TextCard } from "components/TextCard/TextCard";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { NoteAPI } from "api/note-api";
 import { deleteNote } from "store/notes/notes-slice";
 
-export function NoteList() {
-  const noteList = useSelector((store) => store.noteSlice.noteList);
+export function NoteList({ noteList }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,12 +1,35 @@
+import { SearchBar } from "components/SearchBar/SearchBar";
 import { NoteList } from "containers/NoteList/NoteList";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export function NoteBrowse() {
   const noteList = useSelector((store) => store.noteSlice.noteList);
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredNoteList = noteList.filter((note) => {
+    const containsTitle = note.title
+      .trim()
+      .toUpperCase()
+      .includes(searchTerm.trim().toUpperCase());
+    const containsContent = note.content
+      .trim()
+      .toUpperCase()
+      .includes(searchTerm.trim().toUpperCase());
+    return containsTitle || containsContent;
+  });
 
   return (
     <>
+      <div className="row justify-content-center mb-5">
+        <div className="col-sm-12 col-md-4">
+          <SearchBar
+            onTextChange={setSearchTerm}
+            placeholder="Search your notes"
+          />
+        </div>
+      </div>
+
       {noteList?.length === 0 && (
         <div className="d-flex justify-content-center">
           <span>
@@ -15,7 +38,7 @@ export function NoteBrowse() {
           </span>
         </div>
       )}
-      <NoteList />
+      <NoteList noteList={filteredNoteList} />
     </>
   );
 }
